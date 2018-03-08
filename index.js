105fe7f027bb56a361570df740806205ae9cfddf
Jonathan E. Moffie <jonathanmoffie.sinow@gmail.com>
2018-03-08 13:54:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string) {
-  console.log() string.toUpperCase()
-}
-
-function logWhisper(string) {
-  console.log() string.toLowerCase()
-}
 
 function sayHiToGrandma(string) {
   return "I can't hear you!" if 'string' === string.toLowerCase()
